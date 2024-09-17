54ec6167d1bcedfd2f3284c0387a11344fb36d71
mariasimoesalmeida <dudasimoesalmeida12@icloud.com>
2024-09-17 12:52:20 -0300

    definiu a classe user controller , ultilizou o padrão singleton

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,4 +1,14 @@
  <?php
  //Definição da classe UserController, responsavel por gerenciar as ações relacionadas aos usuários
- class
+ class UserController
+ {
+    public function register(){
+        $data = [
+            'nome' => $_POST['nome'],
+            'email' => $_POST['email'],
+            'senha' => password_hash($_POST['senha'],
+            'perfil' => $_POST['perfil'],
+        ]
+    }
+ }
  ?>
---
+++ models/database.php
@@ -0,0 +1,5 @@
+<?php
+class Database{
+   //Ultiliza padrão Singleton, cujo objetivo é garantir que apenas uma única instância de classe seja criada durante a execução do programa, e que essa instância seja ultilizado sempre que o nescessário
+}
+?>
